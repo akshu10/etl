cf99d2d9b896a9bb723b71c1d716e407d75f197e
Parth Patel <58887946+akshu10@users.noreply.github.com>
2022-01-08 18:01:08 -0400

    Updates

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-import express from "express";
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello There!");
-});
-
-app.post("/table", (req, res) => {});
-
-const port = 8090;
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+import { createTable } from "./create-table";
